0dbed59989406473b90e53bc388f122acf4e7608
ramire-uces <nicolasr.renterias@gmail.com>
2025-04-28 21:57:06 -0300

    Delete JuegoSuperMiniRPG directory

--- JuegoSuperMiniRPG/interfaces/IEstaVivo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ActividadIntegradora {
-    public interface IEstaVivo {
-        int Health { get; }
-        void RecibirDamage(int damage);
-        bool EstaVivo(); 
-    } 
-}
