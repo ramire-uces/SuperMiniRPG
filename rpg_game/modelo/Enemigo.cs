dc707e541c946e72fd5b09f2cc033a05cff0bebb
ramire-uces <nicolasr.renterias@gmail.com>
2025-04-28 21:59:04 -0300

    Add files via upload

---
+++ rpg_game/modelo/Enemigo.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace ActividadIntegradora {
+    public abstract class Enemigo : IAtaca, IEstaVivo {
+        private int health;
+        private int attackDamage;
+
+        public int Health {
+            get { return health; }
+            protected set { health = value; }
+        }
+
+        public int AttackDamage {
+            get { return attackDamage; }
+            protected set { attackDamage = value; }
+        }
+
+        public Enemigo() {}
+
+        public abstract void Atacar(IEstaVivo objetivo);
+
+        public void RecibirDamage(int damage) {
+            Health -= damage;
+            if (Health < 0) Health = 0;
+        }
+
+        public bool EstaVivo() {
+            return Health > 0;
+        }
+
+        public override string ToString() {
+            return $"Enemigo. Vida: {Health}, Daño de ataque: {AttackDamage}";
+        }
+    }
+}
+
+
+
+
---
+++ rpg_game/modelo/Orco.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ActividadIntegradora {
+    public class Orco : Enemigo {
+        public Orco() {
+            Health = 40;
+            AttackDamage = 3; 
+        }
+
+        public override void Atacar(IEstaVivo objetivo) {
+            objetivo.RecibirDamage(AttackDamage);
+        }
+    }
+}
