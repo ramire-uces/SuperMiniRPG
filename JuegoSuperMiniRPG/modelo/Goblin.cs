43bc40a27d3a8f7835d23f53c4adb626f53bb653
ramire-uces <nicolasr.renterias@gmail.com>
2025-04-28 21:55:54 -0300

    Add files via upload

---
+++ JuegoSuperMiniRPG/modelo/Goblin.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ActividadIntegradora {
+    public class Goblin : Enemigo {
+        public Goblin() {
+            Health = 15;
+            AttackDamage = 2;
+        }
+
+        public override void Atacar(IEstaVivo objetivo) {
+            objetivo.RecibirDamage(AttackDamage);
+        }
+    }
+}
